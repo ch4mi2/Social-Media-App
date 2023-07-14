6cf1daf75a7e8eb18ef563ed085f7cdae834cd15
ch4mi2 <chamithu4@gmail.com>
2023-07-14 21:06:21 +0530

    format

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,23 +1,49 @@
 const LoginForm = () => {
-    return (
-        <div className="flex items-center justify-center">
-          <div className="bg-white p-8 rounded-3xl shadow-md w-96">
-            <h2 className="text-2xl font-bold mb-4">Login</h2>
-            <form>
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">Email</label>
-                <input type="email" id="email" name="email" className="w-full px-3 py-2 border border-gray-300 rounded-3xl focus:outline-none focus:border-indigo-500" required />
-              </div>
-              <div className="mb-6">
-                <label htmlFor="password" className="block text-gray-700 font-semibold mb-2">Password</label>
-                <input type="password" id="password" name="password" className="w-full px-3 py-2 border border-gray-300 rounded-3xl focus:outline-none focus:border-indigo-500" required />
-              </div>
-              <button type="submit" className="w-full py-2 px-4 bg-indigo-500 text-white font-semibold rounded-3xl hover:bg-indigo-600">Login</button>
-            </form>
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-white p-8 rounded-3xl shadow-md w-96">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full px-3 py-2 border border-gray-300 rounded-3xl focus:outline-none focus:border-indigo-500"
+              required
+            />
           </div>
-        </div>
-      );
-      
-}
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block text-gray-700 font-semibold mb-2"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full px-3 py-2 border border-gray-300 rounded-3xl focus:outline-none focus:border-indigo-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-indigo-500 text-white font-semibold rounded-3xl hover:bg-indigo-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default LoginForm;
